93b5a8cc54e256778e9bdca095f963045c45271a
Natalia Di Paolo <natydipa@gmail.com>
2026-06-11 16:26:34 -0300

    feat: add product contract validation with pydantic

--- tests/products/test_get_product.py
+++ tests/products/test_get_product.py
@@ -1,9 +1,24 @@
+from src.models.product_model import ProductModel
+
 def test_get_product_by_id(products_client):
 
     response = products_client.get_product(1)
 
     assert response.status_code == 200
 
-    product = response.json()
+    product = ProductModel.model_validate(
+        response.json()
+    )
+    assert product.id == 1
 
-    assert product["id"] == 1
+def test_get_all_products(products_client):
+    response = products_client.get_all_products()
+
+    assert response.status_code == 200
+
+    products = [
+        ProductModel.model_validate(product)
+        for product in response.json()
+   ]
+
+    assert len(products) > 0 
